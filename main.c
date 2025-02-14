64d8ca642c8cf208c60972ea3c44fcdcb4bb17c5
Elena <eparapounsky@gmail.com>
2025-02-14 20:38:57 +0000

    begin to adapt sample parser

--- main.c
+++ main.c
@@ -1,27 +1,48 @@
 #include <stdio.h> // for printf
 #include <string.h> // for strtok_r
+#include <stdbool.h> // for true/false
 
 // macros
 #define COMMAND_LINE_SIZE 2048
+#define MAX_ARGS 512
+
+// function prototypes
+int exit_program();
+
+// structs
+struct user_command {
+	char* argv[MAX_ARGS +1];
+	int argc;
+	char* input_file;
+	char* output_file;
+	bool is_background;
+};
+
+struct user_command* parse_command() {
+	char user_input[COMMAND_LINE_SIZE];
+	struct user_command* current_command = (struct command_line*) calloc(1, sizeof(struct user_command));
+}
 
 int main () {
-	// get user's command
-	char user_command[COMMAND_LINE_SIZE];
-	printf(": ");
-	fgets(user_command, COMMAND_LINE_SIZE, stdin); // read from standard input
-
-	// parse user's command
-	char* token = NULL;
-	char* remainder = user_command;
-
-	// strtok_r returns NULL when there are no more tokens
-	while ((token = strtok_r(remainder, " ", &remainder)) != NULL) {
-		if (strcmp(token, "exit") == 0) {
-
-		}
-	}
-
+//	// get user's command
+//	char user_command[COMMAND_LINE_SIZE];
+//	printf(": ");
+//	fgets(user_command, COMMAND_LINE_SIZE, stdin); // read from standard input
+//
+//	// parse user's command
+//	char* token = NULL;
+//	char* remainder = user_command;
+//
+//	// strtok_r returns NULL when there are no more tokens
+//	while ((token = strtok_r(remainder, " ", &remainder)) != NULL) {
+//		if (strcmp(token, "exit") == 0) {
+//			exit_program();
+//		}
+//	}
 
 	return 0;
 }
 
+int exit_program() {
+	return 0;
+}
