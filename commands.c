287a5d252b1dec36881eb12a4008c16ac7eddd4c
Elena <eparapounsky@gmail.com>
2025-02-16 01:41:15 +0000

    fix typo

--- commands.c
+++ commands.c
@@ -3,12 +3,12 @@
 
 // built-in exit command
 void exit_program() {
-	terimnate_processes();
+	terminate_processes();
 	exit(0);
 }
 
 // built in cd command
-void change_directory() {}
+//void change_directory() {}
 
 // built in status command
-int print_status() {}
+//int print_status() {}
