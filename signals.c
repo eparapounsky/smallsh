2b3cae13d2a90951507dd0046fe95c70fccb3ab8
Elena <eparapounsky@gmail.com>
2025-02-23 19:12:13 +0000

    begin implementing register_signal_handlers

--- signals.c
+++ signals.c
@@ -1,9 +1,17 @@
 #include "signals.h"
 
 /**
- * Signal handler for SIGINT.
- * @param
+ * Signal handler for SIGINT (CTRL-C).
+ * The parent process and any children running as background processes ignore SIGINT.
+ * A child running as a foreground process terminates itself upon receiving SIGINT (default action).
+ * @param signal_number: int, the integer code representing SIGINT
  * Adapted from Module 7: Signal Handling API code
  */
 
 void handle_SIGINT (int signal_number) {}
+
+/**
+ *
+ * Adapted from Module 7: Signal Handling API code
+ */
+void register_signal_handlers() {}
