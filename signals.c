89fabc4e4965ed76234bbe7b1a753f2363e6dee4
Elena <eparapounsky@gmail.com>
2025-02-24 01:15:00 +0000

    fix SIGSTP typo (should be SIGTSTP)

--- signals.c
+++ signals.c
@@ -12,19 +12,26 @@ void handle_SIGINT (int signal_number) {
 }
 
 /**
- *
+ * Signal handler for SIGTSTP (CTRL-Z).
+ * All child processes (background or foreground) ignore SIGTSTP.
+ * SIGTSTP tells the parents process to enable or disable running commands in the foreground only.
+ * @param signal_number: int, the integer code representing SIGTSTP
  */
-void handle_SIGSTP (int signal_number) {}
+void handle_SIGTSTP (int signal_number) {}
 
 /**
  *
  * Adapted from Module 7: Signal Handling API code
  */
 void register_signal_handlers() {
+	// SIGINT
 	struct sigaction SIGINT_action = {0}; // initialize to be empty
-
 	// fill out the SIGINT_action struct
 	SIGINT_action.sa_handler = handle_SIGINT; // register handle_SIGINT as the signal handler
-
 	sigaction(SIGINT, &SIGINT_action, NULL); // install signal handler
+
+	// SIGTSTP
+	struct sigaction SIGTSTP_action = {0};
+	SIGTSTP_action.sa_handler = handle_SIGTSTP;
+	sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 }
