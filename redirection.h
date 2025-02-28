3e79e87bcf017b686ac8323da2838a71d328f679
Elena <eparapounsky@gmail.com>
2025-02-28 19:46:52 +0000

    include needed header files

--- redirection.h
+++ redirection.h
@@ -1,6 +1,10 @@
 #ifndef REDIRECTION_H
 #define REDIRECTION_H
 
+#include <fcntl.h> // for open
+#include <unistd.h> // for dup2, close
+#include "main.h"
+
 // function prototypes
 void handle_input_redirection(struct user_command* current_command);
 void handle_output_redirection(struct user_command* current_command);
