c3c41ec50934d70219fd1e2743c00c909b03f430
Elena <eparapounsky@gmail.com>
2025-02-28 19:33:30 +0000

    create header file for redirection.c

---
+++ redirection.h
@@ -0,0 +1,8 @@
+#ifndef REDIRECTION_H
+#define REDIRECTION_H
+
+// function prototypes
+void handle_input_redirection(struct user_command* current_command);
+void handle_output_redirection(struct user_command* current_command);
+
+#endif // REDIRECTION_H
