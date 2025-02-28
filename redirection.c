7367447a1dd3bb14ad90e45c28dff3f1aabf33c8
Elena <eparapounsky@gmail.com>
2025-02-28 19:46:41 +0000

    implement handle_input_redirection and handle_output_redirection

--- redirection.c
+++ redirection.c
@@ -1,13 +1,80 @@
 #include "redirection.h"
+#include "main.h"
 
 /**
  *
  * @param current_command: user_command struct, the current command being executed
  */
-void handle_input_redirection(struct user_command* current_command) {}
+void handle_input_redirection(struct user_command* current_command) {
+	if (current_command->input_file) {
+		char* input_file = current_command->input_file;
+		int input_file_fd = open(input_file, O_RDONLY);
+
+		// check if opening the file failed
+		if (input_file_fd == -1) {
+			printf("\ncannot open %s for input\n", input_file);
+			exit(1);
+		}
+
+		// redirect stdin to input file
+		int result = dup2(input_file_fd, STDIN_FILENO);
+
+
+		if (result == -1) {
+			perror("dup2");
+			exit(2);
+		}
+		close(input_file_fd);
+	} else if (current_command->is_background_process) { // background process with no input redirection specified
+		int dev_null_fd = open("/dev/null", O_RDONLY);
+
+		// redirect stdin to /dev/null
+		int result = dup2(dev_null_fd, STDIN_FILENO);
+
+		// check if redirection failed
+		if (result == -1) {
+			perror("dup2");
+			exit(2);
+		}
+		close(dev_null_fd);
+	}
+}
 
 /**
  *
  * @param current_command: user_command struct, the current command being executed
  */
-void handle_output_redirection(struct user_command* current_command) {}
+void handle_output_redirection(struct user_command* current_command) {
+	if (current_command->output_file) {
+		char* output_file = current_command->output_file;
+		int output_file_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644); // rw-r--r--
+
+		// check if opening the file failed
+		if (output_file_fd == -1) {
+			printf("\ncannot open %s for output\n", output_file);
+			exit(1);
+		}
+
+		// redirect stdout to output file
+		int result = dup2(output_file_fd, STDOUT_FILENO);
+
+		// check if redirection failed
+		if (result == -1) {
+			perror("dup2");
+			exit(2);
+		}
+		close(output_file_fd);
+	} else if (current_command->is_background_process) { // background process with no output redirection specified
+		int dev_null_fd = open("/dev/null", O_RDONLY);
+
+		// redirect stdout to /dev/null
+		int result = dup2(dev_null_fd, STDOUT_FILENO);
+
+		// if redirection failed
+		if (result == -1) {
+			perror("dup2");
+			exit(2);
+		}
+		close(dev_null_fd);
+	}
+}
