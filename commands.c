043531434a45dd6995ed5ebb65d37db86f216dd5
Elena <eparapounsky@gmail.com>
2025-02-28 08:07:50 +0000

    replace child process with handle_child_process

--- commands.c
+++ commands.c
@@ -50,9 +50,6 @@ void other_commands(struct user_command* current_command) {
 	pid_t child_PID = fork(); // spawn child process
 	int child_status;
 
-	char* command = current_command->argv[0];
-	char** command_array = current_command->argv;
-
 	switch (child_PID) {
 	case -1: // check if forking failed
 		perror("fork() failed\n");
@@ -60,85 +57,8 @@ void other_commands(struct user_command* current_command) {
 		break;
 
 	case 0: // child process
-		// determine if the child should run in the background
-		bool is_background_process = current_command->is_background_process && !is_foreground_only_mode();
+		handle_child_process(current_command);
 
-		register_child_signal_handlers(is_background_process);
-
-		// input redirection
-		if (current_command->input_file) {
-			char* input_file = current_command->input_file;
-			int input_file_fd = open(input_file, O_RDONLY);
-
-			if (input_file_fd == -1) { // if opening the file failed
-				printf("\ncannot open %s for input\n", input_file);
-				exit(1);
-			}
-
-			// redirect stdin to input file
-			int result = dup2(input_file_fd, STDIN_FILENO);
-
-			if (result == -1) { // if redirection failed
-				perror("dup2");
-				exit(2);
-			}
-
-			close(input_file_fd);
-		} else if (current_command->is_background_process) { // background process with no input redirection specified
-			int dev_null_fd = open("/dev/null", O_RDONLY);
-
-			// redirect stdin to /dev/null
-			int result = dup2(dev_null_fd, STDIN_FILENO);
-
-			if (result == -1) { // if redirection failed
-				perror("dup2");
-				exit(2);
-			}
-
-			close(dev_null_fd);
-		}
-
-		// output redirection
-		if (current_command->output_file) {
-			char* output_file = current_command->output_file;
-			int output_file_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644); // rw-r--r--
-
-			if (output_file_fd == -1) { // if opening the file failed
-				printf("\ncannot open %s for output\n", output_file);
-				exit(1);
-			}
-
-			// redirect stdout to output file
-			int result = dup2(output_file_fd, STDOUT_FILENO);
-
-			if (result == -1) { // if redirection failed
-				perror("dup2");
-				exit(2);
-			}
-
-			close(output_file_fd);
-		} else if (current_command->is_background_process) { // background process with no output redirection specified
-			int dev_null_fd = open("/dev/null", O_RDONLY);
-
-			// redirect stdout to /dev/null
-			int result = dup2(dev_null_fd, STDOUT_FILENO);
-
-			if (result == -1) { // if redirection failed
-				perror("dup2");
-				exit(2);
-			}
-
-			close(dev_null_fd);
-		}
-
-		if (command != NULL && command_array != NULL) {
-			int status = execvp(command, command_array); // replace child with new program (search in PATH variable)
-
-			if (status == -1) { // if execvp returns, error occurred (shell did not find command to run)
-				fprintf(stderr, "\n%s: command not found\n", command);
-				exit(1);
-			}
-		}
 		_exit(0); // terminate child process
 
 	default: // parent process
