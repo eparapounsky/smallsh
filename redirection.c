82ad082d9413a2d7d7306a4039328c2b6702ccff
Elena <eparapounsky@gmail.com>
2025-02-28 19:33:45 +0000

    create new .c file for input/output redirection

---
+++ redirection.c
@@ -0,0 +1,13 @@
+#include "redirection.h"
+
+/**
+ *
+ * @param current_command: user_command struct, the current command being executed
+ */
+void handle_input_redirection(struct user_command* current_command) {}
+
+/**
+ *
+ * @param current_command: user_command struct, the current command being executed
+ */
+void handle_output_redirection(struct user_command* current_command) {}
