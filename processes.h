5723e0a1d6255b330a994a79ed2320355f8e41f7
Elena <eparapounsky@gmail.com>
2025-02-28 08:06:59 +0000

    add needed header files for refactoring

--- processes.h
+++ processes.h
@@ -2,6 +2,12 @@
 #include <stdio.h> // for printf
 #include <signal.h> // for signals
 #include <sys/wait.h> // for WIFEXITED, WIFSIGNALED, WEXITSTATUS, WTERMSIG, waitpid
+#include <fcntl.h> // for open
+#include <unistd.h> // for execvp, dup2, close
+#include <stdbool.h> // for true/false
+#include <sys/wait.h> // for WIFEXITED, WIFSIGNALED, waitpid
+//#include <stdlib.h>
+#include "main.h"
 
 // macros
 #define BACKGROUND_PROCESS_LIMIT 200
@@ -10,6 +16,7 @@
 void add_process(pid_t pid);
 void terminate_processes();
 void check_background_processes();
+void handle_child_process(struct user_command* current_command);
 
 // declare global variables
 extern pid_t background_processes[BACKGROUND_PROCESS_LIMIT];
